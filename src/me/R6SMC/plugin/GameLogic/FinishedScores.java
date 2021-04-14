81940777ef0f39a85caa8fe2b0bab1a732637e29
TheBigCringeMaster <kristiandgrey@gmail.com>
2021-04-14 12:00:11 +0100

    fixing getting endString

--- src/me/R6SMC/plugin/GameLogic/FinishedScores.java
+++ src/me/R6SMC/plugin/GameLogic/FinishedScores.java
@@ -1,5 +1,6 @@
 package me.R6SMC.plugin.GameLogic;
 
+import me.R6SMC.plugin.DevConsole.DevConsole;
 import org.bukkit.ChatColor;
 import org.bukkit.craftbukkit.libs.jline.internal.Nullable;
 import org.bukkit.entity.Player;
@@ -9,9 +10,18 @@ import java.util.List;
 public class FinishedScores {
 
     public static String GetEndResults(@Nullable List<String> PlayerNames){//ASSUMING THAT THE LIST IS SORTED
-        String endResult = ChatColor.DARK_GREEN +  "-----END RESULTS-----\n" +
-                                  ChatColor.GOLD + "#1. "+PlayerNames.get(0)
-                                +ChatColor.GREEN +"#2." +  PlayerNames.get(1);
+        String endResult = "";
+        try {
+            endResult = ChatColor.DARK_GREEN + "-----END RESULTS-----\n" +
+                    ChatColor.GOLD + "#1. " + PlayerNames.get(0) + "\n"
+                    + ChatColor.GREEN + "#2." + PlayerNames.get(1) + "\n"
+                    + ChatColor.GREEN + "#2." + PlayerNames.get(2) + "\n"
+                    + ChatColor.GREEN + "#2." + PlayerNames.get(3) + "\n"
+                    + ChatColor.DARK_GREEN + "-----END RESULTS-----\n";
+
+            return endResult;
+        }catch (Exception e){
+        }
         return endResult;
     }
 }
